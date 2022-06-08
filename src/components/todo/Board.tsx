de33a0916e6e9a5a16a5bc3a74153d35683aa0c0
jmpark-JYG <jmpark@yookgak.com>
2022-06-08 12:28:44 +0900

    update: dragging style 추가

--- src/components/todo/Board.tsx
+++ src/components/todo/Board.tsx
@@ -7,18 +7,28 @@ interface BoardProps {
   todos: string[];
 }
 
+interface AreaProps {
+  isDraggingFromThisWith: boolean;
+  isDraggingOver: boolean;
+}
+
 function Board({ boardId, todos }: BoardProps) {
   return (
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
+        {(provided, snapshot) => (
+          <Area
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThisWith={Boolean(snapshot.draggingFromThisWith)}
+          >
             {todos.map((todo, index) => (
               <DraggableCard key={index} index={index} todo={todo} />
             ))}
             {provided.placeholder}
-          </div>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
@@ -28,6 +38,8 @@ function Board({ boardId, todos }: BoardProps) {
 const Wrapper = styled.div`
   width: 300px;
   min-height: 250px;
+  display: flex;
+  flex-direction: column;
   padding: 10px 10px 20px;
   background-color: #dadfe9;
   border-radius: 5px;
@@ -38,6 +50,13 @@ const Title = styled.h2`
   font-weight: 600;
   margin-bottom: 10px;
   font-size: 18px;
+`;
+
+const Area = styled.div<AreaProps>`
+  padding: 20px;
+  background-color: ${(props) => (props.isDraggingOver ? "pink" : props.isDraggingFromThisWith ? "#b2bec3" : "transparent")};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
 `;
 
 export default Board;
